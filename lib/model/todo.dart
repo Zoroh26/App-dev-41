49a6923f9c983ff458fcfe784a04154b3331778c
rohith <rohith26p@gmail.com>
2023-10-25 00:14:20 +0530

    my changes

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -2,21 +2,16 @@ class ToDo {
   String? id;
   String? todoText;
   bool isDone;
+  int coinsAwarded;
 
   ToDo({
     required this.id,
-    required this.todoText,
+    this.todoText,
     this.isDone = false,
+    this.coinsAwarded = 0,
   });
 
   static List<ToDo> todoList() {
-    return [
-      ToDo(id: '01', todoText: 'Swimming', isDone: true ),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true ),
-      ToDo(id: '03', todoText: 'basketball', ),
-      ToDo(id: '04', todoText: 'GYM', ),
-      ToDo(id: '05', todoText: 'Dev/Track meeting', ),
-      ToDo(id: '06', todoText: 'College assignment', ),
-    ];
+    return <ToDo>[];
   }
 }
